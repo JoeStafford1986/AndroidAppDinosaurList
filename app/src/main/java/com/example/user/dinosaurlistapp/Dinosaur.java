5ac26beea616ca83013f7fb6764a6fd4c9c931de
Joe Stafford <joestafford1986@gmail.com>
2018-05-30 15:18:15 +0100

    added getters for dinosaur class, tests pass

--- app/src/main/java/com/example/user/dinosaurlistapp/Dinosaur.java
+++ app/src/main/java/com/example/user/dinosaurlistapp/Dinosaur.java
@@ -5,4 +5,25 @@ package com.example.user.dinosaurlistapp;
  */
 
 public class Dinosaur {
+    private String name;
+    private double length;
+    private double weight;
+
+    public Dinosaur(String name, double length, double weight) {
+        this.name = name;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
 }
--- app/src/test/java/com/example/user/dinosaurlistapp/DinosaurTest.java
+++ app/src/test/java/com/example/user/dinosaurlistapp/DinosaurTest.java
@@ -1,4 +1,30 @@
 package com.example.user.dinosaurlistapp;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DinosaurTest {
+    Dinosaur dinosaur;
+
+    @Before
+    public void before() {
+        dinosaur = new Dinosaur("Allosaurus", 12.0, 3.0);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Allosaurus", dinosaur.getName());
+    }
+
+    @Test
+    public void canGetLength() {
+        assertEquals(12, dinosaur.getLength(), 0.1);
+    }
+
+    @Test
+    public void canGetWeight() {
+        assertEquals(3, dinosaur.getWeight(), 0.1);
+    }
 }
