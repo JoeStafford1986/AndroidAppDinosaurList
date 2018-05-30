9a8f4831033c1c2e0c98c0cf55e01c180a6fc7e0
Joe Stafford <joestafford1986@gmail.com>
2018-05-30 15:35:03 +0100

    added default list to dinosaurlist, created getter, test passes

--- app/src/main/java/com/example/user/dinosaurlistapp/DinosaurList.java
+++ app/src/main/java/com/example/user/dinosaurlistapp/DinosaurList.java
@@ -1,8 +1,31 @@
 package com.example.user.dinosaurlistapp;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+
 /**
  * Created by user on 30/05/2018.
  */
 
 public class DinosaurList {
+    private ArrayList<Dinosaur> list;
+
+    public DinosaurList() {
+        list = new ArrayList<>();
+        list.add(new Dinosaur("Allosaurus", 12, 3));
+        list.add(new Dinosaur("Ankylosaurus", 10, 5));
+        list.add(new Dinosaur("Apatosaurus", 23, 25));
+        list.add(new Dinosaur("Brachiosaurus", 25, 40));
+        list.add(new Dinosaur("Carnotaurus", 9, 1.5));
+        list.add(new Dinosaur("Diplodocus", 25, 20));
+        list.add(new Dinosaur("Giganotosaurus", 16, 8));
+        list.add(new Dinosaur("Megalosaurus", 9, 1));
+        list.add(new Dinosaur("Ornithomimus", 3.5, 0.5));
+        list.add(new Dinosaur("Parasaurolophus", 10, 3.5));
+        list.add(new Dinosaur("Spinosaurus", 15, 23));
+    }
+
+    public ArrayList<Dinosaur> getList() {
+        return list;
+    }
 }
--- app/src/test/java/com/example/user/dinosaurlistapp/DinosaurListTest.java
+++ app/src/test/java/com/example/user/dinosaurlistapp/DinosaurListTest.java
@@ -1,4 +1,14 @@
 package com.example.user.dinosaurlistapp;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DinosaurListTest {
+
+    @Test
+    public void canGetDinosaurList() {
+        DinosaurList dinosaurList = new DinosaurList();
+        assertEquals(11, dinosaurList.getList().size());
+    }
 }
